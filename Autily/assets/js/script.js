7bc2950cdcb36e995ca08660abe6086424fe9dd4
Daniel Da Silva <100931944+projdanielsilva@users.noreply.github.com>
2024-10-28 22:11:48 -0300

    Add files via upload

---
+++ Autily/assets/js/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () { navbar.classList.toggle("active"); }
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () { navbar.classList.remove("active"); }
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+// Seleciona o botão
+const toggleThemeBtn = document.getElementById("toggleThemeBtn");
+
+// Adiciona o evento de clique
+toggleThemeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    // Alterna o ícone do botão entre sol e lua
+    if (document.body.classList.contains("dark-theme")) {
+        toggleThemeBtn.textContent = "🌞";
+    } else {
+        toggleThemeBtn.textContent = "🌜";
+    }
+});
